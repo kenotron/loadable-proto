c01170437a59e9c8d96f1f4cda4bcdeb0d3829e6
Ken <ken@oasis.local>
2018-06-13 14:10:23 -0700

    webpack 4 time

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {PrimaryButton} from 'office-ui-fabric-react';
-import {initializeIcons} from '@uifabric/icons';
+import { PrimaryButton } from 'office-ui-fabric-react';
+import { initializeIcons } from '@uifabric/icons';
 initializeIcons();
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-
-          <PrimaryButton menuProps={{
-            items: [{
-              key: 'abc',
-              text: 'hi'
-            },{
-              key: 'absdfc',
-              text: 'hisdfsdfsd'
-            }]
-          }}>hello</PrimaryButton>
-        </p>
+          I'm a happy little button
+          </p>
+        <PrimaryButton menuProps={{
+          items: [{
+            key: 'abc',
+            text: 'hi'
+          }, {
+            key: 'absdfc',
+            text: 'hisdfsdfsd'
+          }]
+        }}>hello</PrimaryButton>
       </div>
     );
   }
